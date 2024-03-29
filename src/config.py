e46d912a31e67ad6a431ec8e35e1eaf3fd263ede
poyu39 <bruce920309@gmail.com>
2024-03-29 14:54:27 +0800

    feat: 新增 env

--- src/config.py
+++ src/config.py
@@ -4,5 +4,12 @@ import os
 CONFIG = None
 WORKDIR = os.path.dirname(os.path.abspath(__file__))
 
-with open(f'{WORKDIR}\\storage\\config.yml', 'r') as file:
-    CONFIG = yaml.safe_load(file)
+if os.path.exists(f'{WORKDIR}\\storage\\config.yml'):
+    with open(f'{WORKDIR}\\storage\\config.yml', 'r') as file:
+        CONFIG = yaml.safe_load(file)
+else:
+    CONFIG = {
+        'user': os.environ.get('GITEA_USER'),
+        'url': os.environ.get('GITEA_URL'),
+        'token': os.environ.get('GITEA_TOKEN'),
+    }
